1baecbbe3b64c4b49043ae858061f7816505772d
Fernando Caldera <fernando@jobbatical.com>
2018-12-04 23:05:32 +0200

    Set basic layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components';
+
+const PageHeader = styled.h1`
+  border-bottom: 1px solid #eaeff1;
+  line-height: 2;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>15 December 2017</h1>
+          <PageHeader>15 December 2017</PageHeader>
         </header>
       </div>
     );
